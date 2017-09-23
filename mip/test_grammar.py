149f44cee45653d8d2f5816c998cdea376ecd107
travleev <anton.travleev@kit.edu>
2017-09-23 19:58:16 +0200

    Added alternative grammar for geom. Currently does not work properly.

---
+++ mip/test_grammar.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sys import argv
+import tatsu
+from importlib import import_module
+from os.path import basename
+
+grammar = open('{}.ebnf'.format(argv[1])).read()
+parser = tatsu.compile(grammar)
+semantics = import_module('grammars.{}'.format(basename(argv[1])))
+
+
+def pprint_dict(d, indent=4, _level=1):
+    i = ' ' * indent * _level
+    res = []
+    # d items:
+    if isinstance(d, dict):
+        iterable = d.iteritems()
+    else:
+        iterable = enumerate(d)
+    for k, v in iterable:
+        if isinstance(v, dict) or isinstance(v, list):
+            line = '{}{}:'.format(i, k)
+            res.append(line)
+            lines = pprint_dict(v, indent=indent, _level=_level+1)
+            res.extend(lines)
+        elif v is not None:
+            line = '{}{}: {}'.format(i, k, v)
+            res.append(line)
+    return res
+
+
+if __name__ == '__main__':
+    from cellcard import get_cards_from_file
+    from parsegeom import normalize
+
+    for n, cc in get_cards_from_file(argv[2]):
+        name, mat, geom, opts = cc
+        if 'like' in geom.lower():
+            continue
+        g = normalize(geom)
+        print '*'*60
+        print repr(g)
+        ast = parser.parse(g, semantics=semantics.Semantics())
+        lines = pprint_dict(ast)
+        print '\n'.join(lines)
+
